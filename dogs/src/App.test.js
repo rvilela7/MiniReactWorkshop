3d8242748d51b9af3e6b46804ca06becde456ff1
RuiVilela <ruivil@gmail.com>
2019-10-14 12:00:40 +0100

    fix

--- dogs/src/App.test.js
+++ dogs/src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 //import dogs from './data/dogs';
-import { exportAllDeclaration } from '@babel/types';
+//import { exportAllDeclaration } from '@babel/types';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
